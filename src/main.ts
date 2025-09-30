2af647ca98d584acab0b1bdf89d1f5b447a7e900
hzswdef <ostelia.group@gmail.com>
2025-09-30 17:40:15 +0300

    #fY4zKHhS: NestJS backend initialization.

---
+++ src/main.ts
@@ -0,0 +1,56 @@
+import { CustomConfigService } from "@core/config/config.service";
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { SwaggerTheme, SwaggerThemeNameEnum } from "swagger-themes";
+
+import { AppModule } from "@/app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(CustomConfigService);
+
+  app.setGlobalPrefix("/api/v1");
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  // Development environment things.
+  if (configService.get("app.environment") === "DEVELOPMENT") {
+    // Setup Swagger.
+    const config = new DocumentBuilder()
+      .setTitle("Eleine API")
+      .setDescription(
+        "<div align='center'><img src='https://i.pinimg.com/736x/49/f1/80/49f18047adca07955f8f3929de06ddff.jpg' width='144' alt='vitalik'></div>",
+      )
+      .setVersion("v1.0")
+      .addSecurity("bearer", {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+        name: "access-token",
+      })
+      .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    const theme = new SwaggerTheme();
+    SwaggerModule.setup("/api/doc", app, documentFactory, {
+      customCss:
+        theme.getBuffer(SwaggerThemeNameEnum.DARK) +
+        ".swagger-ui .topbar { display: none }",
+    });
+  }
+
+  // @TODO: Configure CORS properly.
+  app.enableCors({
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  });
+
+  await app.listen(configService.get("app.port"));
+}
+
+bootstrap();
