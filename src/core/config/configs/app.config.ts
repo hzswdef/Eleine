31c72da914329e4819ca3b16694eab5bf2dcaca3
hzswdef <ostelia.group@gmail.com>
2025-10-06 11:24:04 +0300

    #fY4zKHhS: Added corsOrigins environment variable.

--- src/core/config/configs/app.config.ts
+++ src/core/config/configs/app.config.ts
@@ -7,5 +7,17 @@ export default registerAs(
     ({
       environment: process.env.ENVIRONMENT || "DEVELOPMENT",
       port: parseInt(process.env.PORT || "3000", 10),
+      corsOrigins: (() => {
+        const cors = process.env.CORS_OPTIONS;
+        if (!cors) {
+          return ["http://127.0.0.1:5173", "http://localhost:5173"];
+        }
+
+        if (cors.indexOf(",") !== -1) {
+          return cors.split(",");
+        }
+
+        return cors;
+      })(),
     }) as CustomConfigInterface["app"],
 );
